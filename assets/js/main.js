1558419a6ed7e9e0bc3f9ff978206d7f56cfe5aa
bohlerandre <65367542+bohlerandre@users.noreply.github.com>
2021-01-08 00:45:49 -0300

    Primeiro JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 
+var horaTrab = parseFloat(document.getElementById('horaTrab').value);
+horaTrab.innerHTML = form-range.value;
 
 calculaX = function (){
-    var salarioMes =  parseFloat(document.getElementById('salMes').value) ;
-    var horaTrab = parseFloat(document.getElementById('horaTrab').value); 
+    var salarioMes = parseFloat(document.getElementById('salMes').value);
+    horaTrab = parseFloat(document.getElementById('horaTrab').value); 
     var diaSemana = parseFloat(document.getElementById('diaSemana').value);
-    var custoHora = salarioMes/((horaTrab*diaSemana)*4);
+    var semanaMes = parseFloat(document.getElementById('semanaMes').value);
+    var custoHora = salarioMes/((horaTrab*diaSemana)*semanaMes);
    document.getElementById('custoHora').value = custoHora;
    
 }
