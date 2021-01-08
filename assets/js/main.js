497e7ecafe123fbe58408a1f62e46f07cc9b3448
Carlos <carlos_schmidt07@hotmail.com>
2021-01-08 13:18:42 -0300

    Implementando Range Slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,25 @@
 
-var horaTrab = parseFloat(document.getElementById('horaTrab').value);
-horaTrab.innerHTML = form-range.value;
 
+function mostrarResultado(val) {
+    document.getElementById('horaTrabVal').value=val; 
+  }
+  function mostrarResultado1(val) {
+    document.getElementById('diaSemanaVal').value=val; 
+  }
+  function mostrarResultado2(val) {
+    document.getElementById('semanaMesVal').value=val; 
+  }
+
+
+
+    
 calculaX = function (){
     var salarioMes = parseFloat(document.getElementById('salMes').value);
-    horaTrab = parseFloat(document.getElementById('horaTrab').value); 
+    var horaTrab = parseFloat(document.getElementById('horaTrab').value); 
     var diaSemana = parseFloat(document.getElementById('diaSemana').value);
     var semanaMes = parseFloat(document.getElementById('semanaMes').value);
     var custoHora = salarioMes/((horaTrab*diaSemana)*semanaMes);
    document.getElementById('custoHora').value = custoHora;
+   document.getElementById('semanaMesVal').value = semanaMes;
    
 }
