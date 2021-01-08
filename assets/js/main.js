0b1735d8070323a2998acae4c662016651b64694
Carlos <carlos_schmidt07@hotmail.com>
2021-01-08 14:39:55 -0300

    funcao para 2 decimais no campo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
-
+function carregar(){
+    if(document.getElementById("salMes")){
+        document.getElementById("salMes").addEventListener("change", function(){
+        this.value = parseFloat(this.value).toFixed(2);
+     });
+    }
+}
+ 
 
 function mostrarResultado(val) {
     document.getElementById('horaTrabVal').innerHTML=val; 
@@ -18,7 +25,7 @@ calculaX = function (){
     var horaTrab = parseFloat(document.getElementById('horaTrab').value); 
     var diaSemana = parseFloat(document.getElementById('diaSemana').value);
     var semanaMes = parseFloat(document.getElementById('semanaMes').value);
-    var custoHora = salarioMes/((horaTrab*diaSemana)*semanaMes);
+    var custoHora = (salarioMes/((horaTrab*diaSemana)*semanaMes)).toFixed(2);
    document.getElementById('custoHora').value = custoHora;
    document.getElementById('semanaMesVal').value = semanaMes;
    
